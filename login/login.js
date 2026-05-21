2d91a68ada4a48c0faceb898fd644885485eb906
Nam <namdhptit@gmail.com>
2026-05-21 20:46:46 +0700

    update  & fix login

--- login/login.js
+++ login/login.js
@@ -11,19 +11,44 @@ document
     .getElementById("loginForm")
     .addEventListener("submit", function(event) {
         event.preventDefault();
-        const usernameInput = this.querySelector('input[type="text"]');
 
-        if (usernameInput && usernameInput.value.trim() !== "") {
+        const usernameInput = this.querySelector('input[type="text"]');
+        const passwordInput = this.querySelector('input[type="password"]');
+        const username = localStorage.getItem("username");
+        const password = localStorage.getItem("password");
+
+        if (usernameInput.value === '' || passwordInput.value === '') {
+            alert("Vui lòng nhập đầy đủ!");
+        } else if(usernameInput.value === username && passwordInput.value === password) {
             localStorage.setItem("currentUser", usernameInput.value.trim());
-            window.location.href = "index.html";
+            document.body.innerHTML += `
+            <div style="
+                position:fixed;
+                top:20px;
+                right:20px;
+                background:#4caf50;
+                color:white;
+                padding:15px 25px;
+                border-radius:10px;
+                font-size:18px;
+                z-index:9999;
+            ">
+                Đăng nhập thành công!
+            </div>
+        `;
+        setTimeout(() => {
+            window.location.href = "../main/index.html";
+        }, 1500);
         } else {
-            alert("Vui lòng nhập tên tài khoản!");
+            alert('Sai tài khoản hoặc mật khẩu!')
         }
     });
 document
     .getElementById("registerForm")
     .addEventListener("submit", function(event) {
         event.preventDefault();
+        const usernameInput = this.querySelector('input[type="text"]');
+        const username = usernameInput.value;
         const passwordInputs = this.querySelectorAll('input[type="password"]');
         const password = passwordInputs[0].value;
         const confirmPassword = passwordInputs[1].value;
@@ -35,6 +60,8 @@ document
             passwordInputs[1].focus();
             passwordInputs[1].select();
         } else {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
             alert("Đăng ký tài khoản thành công!");
             showLogin();
         }
